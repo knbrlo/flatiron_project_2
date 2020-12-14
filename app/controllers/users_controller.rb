1ace2ab376f267c94851ab36f3dae04e9da22fb0
knbrlo <kenbrlow@gmail.com>
2020-12-13 20:28:52 -0700

    Built out the ability to delete and make edits to account.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
     # sign up - get
-    get '/signup' do 
+    get "/signup" do 
         if is_logged_in?
             # todo - redirect the user to their page using their id
         else
-            erb :'/users/user_create'
+            erb :"/users/user_create"
         end
     end
 
     # sign up - post
-    post '/signup' do
+    post "/signup" do
         # create a new user from the filled out form
         @new_user = User.new(username: params[:username].strip, email: params[:email].strip, password: params[:password].strip)
 
@@ -23,77 +23,124 @@ class UsersController < ApplicationController
             #assign the session id from the newly created and saved user.
             session[:user_id] = @new_user.id
 
-            redirect to '/'
+            redirect to "/"
         else
             # take them back to the login page
-            redirect to '/login'
+            redirect to "/login"
         end
     end
 
 
     # login - get
-    get '/login' do
+    get "/login" do
         if is_logged_in?
             redirect to "/users/#{active_user.id}"
           else
-            erb :'/users/user_login'
+            erb :"/users/user_login"
           end
     end
 
     # login - post
-    post '/login' do
+    post "/login" do
         @user = User.find_by(username: params[:username].strip, password: params[:password].strip)
 
         # if the user is not nil, then send them to the home page
         if !@user.nil?
             session[:user_id] = @user.id
-            redirect to '/'
+            redirect to "/"
         else
             # take them back / keep them on the login page
-            redirect to '/login'
+            redirect to "/login"
         end
     end
 
 
     # profile - get
-    get '/users/:id' do
+    get "/users/:id" do
         
         # get the user
         @user = User.find_by_id(params[:id])
 
         # make sure they're logged in and the id of the page is equal to the active user
         if is_logged_in? && @user == active_user
-            erb :'/users/user_profile'
+            erb :"/users/user_profile"
         else
-            redirect to '/login'
+            redirect to "/login"
         end
 
     end
 
     # profile edit - get
-    get '/users/:id/edit' do
+    get "/users/:id/edit" do
         
         # get the user
         @user = User.find_by_id(params[:id])
 
         # make sure they're logged in and the id of the page is equal to the active user
         if is_logged_in? && @user == active_user
-            erb :'/users/user_edit'
+            # todo -  would be nice to show a message here that it was saved successfully.
+            erb :"/users/user_edit"
         else
-            redirect to '/login'
+            redirect to "/login"
         end
 
     end
 
+    # profile edit - patch
+    patch "/users/:id" do
+
+        # get all of the changes and assign them to the user
+        @user = User.find_by_id(params[:id])
+        @user.username = params[:username].strip
+        @user.email = params[:email].strip
+        @user.password = params[:password].strip
+
+        # make sure they're logged in, they are the user they're trying
+        # to edit
+        if is_logged_in? && @user == active_user
+            @user.save
+            redirect to "/users/#{@user.id}"
+        else
+            redirect to "/login"
+        end
+    end
+
 
     # log out - get
-    get '/logout' do
+    get "/logout" do
         if is_logged_in?
             # if we're logged in then clear the session to put is in a logged out state
             session.clear
-            redirect to '/'
+            redirect to "/"
         else
-            redirect to '/'
+            redirect to "/"
+        end
+    end
+
+    # delete
+    delete "/users/:id" do
+
+        # get the currently logged in user
+        @user = User.find_by_id(params[:id])
+
+        # make sure they're logged in and they are the user
+        # that they're trying to delete
+        if is_logged_in? && @user == active_user
+
+            # todo - delete their providers they're signed up with
+
+            # todo - delete their shows they're watching.
+
+            # delete their account
+            @user.delete
+
+            # now that they're deleted clear the session and send
+            # them to the home page
+            session.clear
+
+            redirect to "/"
+        else
+            redirect to "/login"
         end
     end
 
