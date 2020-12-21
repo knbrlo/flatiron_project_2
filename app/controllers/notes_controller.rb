9ab236f627d925618554341c69dacb32881cbd54
Ken Barlow <kenbrlow@gmail.com>
2020-12-21 09:57:57 -0700

    Finished the route to show them their own notes.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,16 +5,32 @@ class NotesController < ApplicationController
     # 7 restful routes
 
     #index
-    get '/notes' do
-        # todo - only get their notes
-        binding.pry
+    get '/notes/:id' do
+
+        # find the user id they've requested notes for
+        @requested_notes_user = User.find_by_id(params[:id])
+
+        # compare the userid they're requesting notes for against
+        # the logged in user id
+        if @requested_notes_user.id == active_user.id 
+
+            # query for all notes that have the same id as me.
+            @all_notes = Note.where("user_id = #{active_user.id}")
+            erb :"/notes/index"
+        else
+            
+            # if they're trying to access someone else's notes 
+            # take them back to the main page
+            redirect to "/"
+        end
     end
-
-
 
 
     # #new
     # '/notes/new'
+    get '/notes/new' do
+        
+    end
 
     # #create
     # post '/photos'
